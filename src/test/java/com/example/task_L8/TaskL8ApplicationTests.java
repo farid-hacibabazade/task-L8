176c1a6070d515e584fb254203c5947de3631674
farid-hacibabazade <faridhaci0@gmail.com>
2024-11-15 17:06:15 +0400

    initial commit

---
+++ src/test/java/com/example/task_L8/TaskL8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_L8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskL8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
